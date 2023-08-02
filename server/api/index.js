e2e9668694a947ccc05f872260ba342a3f6e1da4
JordanSucher <jsucher@gmail.com>
2023-08-02 18:32:52 -0400

    figured out basics of web scraping w axios and cheerio

--- server/api/index.js
+++ server/api/index.js
@@ -1,31 +1,68 @@
 const router = require('express').Router()
+// const { getTweets, getCapterraReviews, getG2Reviews, getArticles, getContent } = require('./webscraper.js')
 
 router.use('/users', require('./users'))
 
 router.post('/comparisons', async (req, res, next) => {
   
-  let companyURLs = req.body.companies
-  // upsert companies into companies table
 
-  // make a companies variable
-  let companies = something
+  //[google.com, yahoo.com, etc.]
+  let companyURLs = req.body.companies
+
+  // upsert companies into companies table
+  // how do we get company name?
+  let companies = await companies.bulkCreate(companyURLs)
 
 
   // trigger comparison functions:
     // web scrape a bunch of shit - Eric
-    await webScrape()
+    await webScrape(companies)
     
     // trigger config of llama index based on that shit - Hunter
     let index = await setUpLlamaIndex(companies)
 
     // do a bunch of queries
-    await doQueries(index)
+    let results = await doQueries(index)
+
+    //return stuff to frontend
+    res.json (results)
 })
 
 
-const webScrape = async () => {
-  //do a bunch of web scraping
-  //dont return anything until ur done
+const webScrape = async (companies) => {
+  //when provided an array of companies, do a bunch of web scraping
+
+
+const promises = companies.forEach(async company => {
+  // look for tweets
+  await getTweets(company)
+
+  // get capterra reviews
+  await getCapterraReviews(company)
+  
+  // get g2 reviews
+  await getG2Reviews(company)
+
+  // look for articles on google / crunchbase?
+  await getArticles(company)
+
+  // grab content from the competitor's website
+  await getContent(company)
+
+})
+
+await Promise.all(promises) //wait until all the above promises are done
+
+// what inserting into companyData might look like
+
+  // CompanyData.create({
+  //   companyId: someVariable,
+  //   pageTitle: "test",
+  //   url: "test",
+  //   text: "test",
+  //   type: "test"
+  // })
+  
 }
 
 const setUpLlamaIndex = async (companies) => {
