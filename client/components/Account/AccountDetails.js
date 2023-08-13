52c910c505ac2f501a94b6a2e7146ddb39ddae56
Eston <estoncjunior@gmail.com>
2023-08-12 22:24:53 -0400

    drafted account details comp

--- client/components/Account/AccountDetails.js
+++ client/components/Account/AccountDetails.js
@@ -1,16 +1,74 @@
 import React from "react";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Container, Row, Col, Card, Form } from "react-bootstrap";
 
 const AccountDetails = () => {
   return(
     <>
-    <Container>
-      <Row>
-        <Card>
-          <Card.Title>Account</Card.Title>
-          <Card.Body></Card.Body>
+    <Container className="d-flex justify-content-center">
+        <Card className="w-50">
+
+          <Card.Title className="text-center">Account Details</Card.Title>
+
+          <Card.Body>
+            <Row className="justify-content-center">
+              <Card.Text as={Col}>
+                Name:
+              </Card.Text>
+              <Card.Text as={Col}>
+                Insert Full Name
+              </Card.Text>
+            </Row>
+            <Row className="justify-content-center">
+              <Card.Text as={Col}>
+                Email:
+              </Card.Text>
+              <Card.Text as={Col}>
+                Insert Email
+              </Card.Text>
+            </Row>
+            <Row className="justify-content-center">
+              <Card.Text as={Col}>
+                Company Name:
+              </Card.Text>
+              <Card.Text as={Col}>
+                Insert Company Name
+              </Card.Text>
+            </Row>
+            <Row>
+              <Card.Text as={Col}>
+                Company URL:
+              </Card.Text>
+              <Card.Text as={Col}>
+                Insert Company URL
+              </Card.Text>
+            </Row>
+            <Row>
+              <Card.Text as={Col}>
+                Competitors:
+              </Card.Text>
+              <Card.Text as={Col}>
+                Insert Ordered List of tracked competitors
+              </Card.Text>
+            </Row>
+            <Row>
+              <Card.Text as={Col}>
+                Open API Key:
+              </Card.Text>
+              <Card.Text as={Col}>
+                Insert Open API Key
+              </Card.Text>
+            </Row>
+            <Row>
+              <Form>
+                <Form.Check
+                  type="switch"
+                  id="custom-switch"
+                  label="Weely Email Comparison Updates"
+                />
+              </Form>
+            </Row>
+          </Card.Body>
         </Card>
-      </Row>
     </Container>
     </>
   )
