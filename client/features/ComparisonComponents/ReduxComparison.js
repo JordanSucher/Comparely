f2217d00b5a0391c1a343f88d440a344221b676b
Eston <estoncjunior@gmail.com>
2023-08-20 17:51:22 -0400

    rough ReduxComparison

--- client/features/ComparisonComponents/ReduxComparison.js
+++ client/features/ComparisonComponents/ReduxComparison.js
@@ -1,46 +1,30 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import TableOfContents from "./TableOfContents";
-import { Container, Row, Col, Offcanvas, Button } from "react-bootstrap";
-import axios from "axios";
+import { Container, Row, Offcanvas, Button } from "react-bootstrap";
 import ComparisonTable from "./ComparisonTable";
 import { CaretRight } from "react-bootstrap-icons";
 import { useParams } from "react-router-dom";
 import { getData } from "./comparisonSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const Comparison = () => {
   const dispatch = useDispatch();
   //This is tentative functions to access DB
-  const [data, setData] = useState([]);
   const [show, setShow] = useState(false);
   const [doTypingEffect, setDoTypingEffect] = useState(false);
-
-  const [companyIds, setCompanyIds] = useState([]);
-  const [companyNames, setCompanyNames] = useState({});
-  const [swotNames, setSwotNames] = useState({});
-  const [swotsData, setSwotsData] = useState([]);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   let { comparisonId } = useParams();
+  const data = useSelector((state) => state.comparison.text);
 
   useEffect(() => {
     dispatch(getData())
   },[])
-  // const getData = async () => {
-  //   try {
-  //     console.log("comparisonId", comparisonId);
-  //     const { data } = await axios.get(`/api/comparisons/${comparisonId}`);
-  //     if (data.text) {
-  //       const parsedData = JSON.parse(data.text);
-  //       setData(parsedData);
-  //       // setSwotsData(parsedData.swots);
-  //     }
-  //   } catch (error) {
-  //     console.log(`Error fetching data:`, error);
-  //   }
-  // };
+
+  console.log("received data:", data)
+
 
   useEffect(() => {
     // Initialize SSE connection
@@ -64,93 +48,10 @@ const Comparison = () => {
       evtSource.close();
     };
 
-    getData();
   }, []);
-  console.log("GETDATA swots", swotsData)
 
-  useEffect(() => {
-    // Create an async function
-    const fetchCompanyNames = async () => {
-      // Create an array to store all promises
-      const promises = data.features.map(async (obj) => {
-        if (!companyNames[obj.companyId]) {
-          const { data } = await axios.get("/api/companies/" + obj.companyId);
-          return { id: obj.companyId, name: data.name };
-        }
-        return null; // If the company name already exists, return null
-      });
 
-      // Resolve all promises
-      const results = await Promise.all(promises);
 
-      // Create a new object based on the previous companyNames and the fetched results
-      const newCompanyNames = { ...companyNames };
-      results.forEach((result) => {
-        if (result) {
-          // Check if the result isn't null
-          newCompanyNames[result.id] = toTitleCase(result.name);
-        }
-      });
-
-      // Update the state
-      setCompanyNames(newCompanyNames);
-      console.log(newCompanyNames);
-    };
-
-    // Call the async function
-    if (data.features) {
-      fetchCompanyNames();
-    }
-  }, [data.features]);
-
-  useEffect(() => {
-    // Create an async function
-    const fetchSwotNames = async () => {
-      // Create an array to store all promises
-      const promises = data.swots.map(async (obj) => {
-        if (!companyNames[obj.companyId]) {
-          const { data } = await axios.get("/api/companies/" + obj.companyId);
-          return { id: obj.companyId, name: data.name };
-        }
-        return null; // If the company name already exists, return null
-      });
-
-      // Resolve all promises
-      const results = await Promise.all(promises);
-
-      // Create a new object based on the previous companyNames and the fetched results
-      const newSwotNames = { ...companyNames };
-      results.forEach((result) => {
-        if (result) {
-          // Check if the result isn't null
-          newSwotNames[result.id] = toTitleCase(result.name);
-        }
-      });
-
-      // Update the state
-      setSwotNames(newSwotNames);
-      console.log(newSwotNames);
-    };
-
-    // Call the async function
-    if (data.swots) {
-      fetchSwotNames();
-    }
-  }, [data.swots]);
-
-  function toTitleCase(str) {
-    if (str) {
-      return str
-        .split(" ")
-        .map(function (word) {
-          return word.charAt(0).toUpperCase() + word.slice(1);
-        })
-        .join(" ");
-    }
-  }
-
-  // Get the proper title for comparison
-  const title = Object.values(companyNames).join(" v ");
 
   return (
     <Container fluid>
@@ -164,7 +65,7 @@ const Comparison = () => {
       </Offcanvas>
 
       <Row className="my-5">
-        <h1 className="text-center">{title}</h1>
+        <h1 className="text-center">YOUR COMPANY VS COMPETITORS</h1>
       </Row>
       <Button
         className="border-0"
@@ -184,15 +85,9 @@ const Comparison = () => {
           companyNames={companyNames}
           doTypingEffect={doTypingEffect}
         />
-        <ComparisonTable
-          title={"Swot Analysis"}
-          swots={data.swots}
-          companyNames={swotNames}
-          doTypingEffect={doTypingEffect}
-        />
       </Row>
     </Container>
   );
 };
 
-export default Comparison;
+export default ReduxComparison;
