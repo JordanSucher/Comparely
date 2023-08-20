f026a9bdfe020539b4f059afea4311794effa4a8
Eston <estoncjunior@gmail.com>
2023-08-20 17:43:32 -0400

    started comparisonSlice and draft of ReduxComparisonComponent

--- client/app/store.js
+++ client/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/Account/authSlice";
+import comparisonReducer from "../features/Comparison/ComparisonSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    compare: comparisonReducer,
   },
 });
 
--- client/features/ComparisonComponents/comparisonSlice.js
+++ client/features/ComparisonComponents/comparisonSlice.js
@@ -1,4 +1,29 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const 
+export const getData = createAsyncThunk("comparison/getData", async () => {
+  try {
+    const { data } = await axios.get(`/api/comparisons/${comparisonId}`);
+
+    return data
+
+  } catch (error) {
+    console.log(`Error fetching data:`, error);
+  }
+})
+
+export const comparisonSlice = createSlice({
+  name: 'comparison',
+  initialState: {
+    id: null,
+    text: {}
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getData.fulfilled, (state, action) => {
+      state.text = action.payload;
+    })
+  }
+})
+
+export default comparisonSlice.reducer;
